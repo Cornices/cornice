2d735775d63192bc8e97d9050c6a2ea898583812
Ryan Kelly <ryan@rfk.id.au>
2011-10-14 11:32:21 +1100

    Refactor and cleanup the anything-to-IPSet conversion logic

--- cornice/auth/ipauth.py
+++ cornice/auth/ipauth.py
@@ -9,6 +9,68 @@ from pyramid.security import Everyone, Authenticated
 from netaddr import IPAddress, IPNetwork, IPGlob, IPRange, IPSet
 
 
+class IPAuthenticationPolicy(object):
+    """An IP-based authentication policy for pyramid.
+
+    This pyramid authentication policy assigns userid and/or effective
+    principals based on the originating IP address of the request.
+
+    You must specify a set of IP addresses against which to match, and may
+    specify a userid and/or list of principals to apply.  For example, the
+    following would authenticate all requests from the 192.168.0.* range as
+    userid "myuser":
+
+        IPAuthenticationPolicy(["192.168.0.0/24"], "myuser")
+
+    The following would not authenticate as a particular userid, but would add
+    "local" as an effective principal for the request (along with Everyone
+    and Authenticated):
+
+        IPAuthenticationPolicy(["127.0.0.0/24"], principals=["local"])
+
+    By default this policy does not respect the X-Forwarded-For header since
+    it can be easily spoofed.  If you want to respect X-Forwarded-For then you
+    must specify a list of trusted proxies, and only forwarding declarations
+    from these proxies will be respected:
+
+        IPAuthenticationPolicy(["192.168.0.0/24"], "myuser",
+                               proxies=["192.168.0.2"])
+
+    """
+
+    implements(IAuthenticationPolicy)
+
+    def __init__(self, ipaddrs, userid=None, principals=None, proxies=None):
+        self.ipaddrs = make_ip_set(ipaddrs)
+        self.userid = userid
+        self.principals = principals
+        self.proxies = make_ip_set(proxies)
+
+    def authenticated_userid(self, request):
+        return self.unauthenticated_userid(request)
+
+    def unauthenticated_userid(self, request):
+        if not check_ip_address(request, self.ipaddrs, self.proxies):
+            return None
+        return self.userid
+
+    def effective_principals(self, request):
+        principals = [Everyone]
+        if check_ip_address(request, self.ipaddrs, self.proxies):
+            if self.userid is not None:
+                principals.insert(0, self.userid)
+            principals.append(Authenticated)
+            if self.principals is not None:
+                principals.extend(self.principals)
+        return principals
+
+    def remember(self, request, principal, **kw):
+        pass
+
+    def forget(self, request):
+        pass
+
+
 def get_ip_address(request, proxies=None):
     """Get the originating IP address from the given request.
 
@@ -25,7 +87,7 @@ def get_ip_address(request, proxies=None):
     if proxies is None:
         proxies = IPSet()
     elif not isinstance(proxies, IPSet):
-        proxies = parse_ip_set(proxies)
+        proxies = make_ip_set(proxies)
     # Get the chain of proxied IP addresses, most recent proxy last.
     addr_chain = []
     try:
@@ -53,143 +115,106 @@ def check_ip_address(request, ipaddrs, proxies=None):
     used to determine the originating IP as per the get_ip_address function.
     """
     if not isinstance(ipaddrs, IPSet):
-        ipaddrs = parse_ip_set(ipaddrs)
+        ipaddrs = make_ip_set(ipaddrs)
     ipaddr = get_ip_address(request, proxies)
     return (ipaddr in ipaddrs)
 
 
-_COMMA_OR_WHITESPACE = re.compile(r"[,\s]")
+#  This is used to split a string on an optional comma,
+#  followed by any amount of whitespace.
+_COMMA_OR_WHITESPACE = re.compile(r",?\s*")
 
-def parse_ip_set(ipaddrs):
+
+def make_ip_set(ipaddrs):
     """Parse a variety of IP specifications into an IPSet object.
 
     This is a convenience function that allows you to specify a set of
     IP addresses in a variety of ways:
 
         * as an IPSet, IPAddress, IPNetwork, IPGlob or IPRange object
-        * as a string parsable by any of the above:
-            * A.B.C.D            <- IPAddress
-            * A.B.C.D/N          <- IPNetwork
-            * A.B.C.*            <- IPGlob
-            * A.B.C.D-A.B.C.E    <- IPRange
+        * as the literal None for the empty set
         * as an int parsable by IPAddress
-        * as the special strings "local" or "all"
-        * the literal None for the empty set
-        * as a whitespace- or comma-separated string of IP specifications
+        * as a string parsable by make_ip_set
         * as an iterable of IP specifications
 
     """
-    # convert other things into one of the netaddr types
+    # If it's already an IPSet, well, that's easy.
+    if isinstance(ipaddrs, IPSet):
+        return ipaddrs
+    # None represents the empty set.
     if ipaddrs is None:
-        ipaddrs = IPSet()
-    elif isinstance(ipaddrs, (int, long)):
-        ipaddrs = IPAddress(ipaddrs)
-    elif isinstance(ipaddrs, basestring):
-        if ipaddrs == "local":
-            ipaddrs = IPSet((IPNetwork("127.0.0.0/8"),))
-            # XXX: can we enumerate all interfaces on the machine?
-            # or should this just be the interface the server is on?
-            localaddrs = socket.gethostbyname_ex(socket.gethostname())[2]
-            for localaddr in localaddrs:
-                ipaddrs.add(IPAddress(localaddr))
-        elif ipaddrs == "all":
-            ipaddrs = IPSet((IPNetwork("0.0.0.0/0"),))
-            ipaddrs.add(IPNetwork("::"))
-        else:
-            ipspecs = _COMMA_OR_WHITESPACE.split(ipaddrs)
-            ipaddrs = IPSet()
-            for ipspec in ipspecs:
-                ipspec = ipspec.strip()
-                if ipspec:
-                    if "/" in ipspec:
-                        ipaddrs.add(IPNetwork(ipspec))
-                    elif "*" in ipspec:
-                        for cidr in IPGlob(ipspec).cidrs():
-                            ipaddrs.add(cidr)
-                    elif "-" in ipspec:
-                        start, end = ipspec.split("-", 1)
-                        for cidr in IPRange(start, end).cidrs():
-                            ipaddrs.add(cidr)
-                    else:
-                        ipaddrs.add(IPAddress(ipspec))
-    # convert the other netaddr types into an IPSet
+        return IPSet()
+    # Integers represent a single address.
+    if isinstance(ipaddrs, (int, long)):
+        return IPSet((IPAddress(ipaddrs),))
+    # Strings get parsed as per make_ip_set
+    if isinstance(ipaddrs, basestring):
+        return parse_ip_set(ipaddrs)
+    # Other netaddr types can be converted into a set.
     if isinstance(ipaddrs, (IPAddress, IPNetwork)):
         return IPSet((ipaddrs,))
     if isinstance(ipaddrs, (IPGlob, IPRange)):
         return IPSet(ipaddrs.cidrs())
-    if isinstance(ipaddrs, IPSet):
-        return ipaddrs
-    # if it's a generic iterable, try to map over it
+    # Anything iterable can be mapped over and unioned.
     try:
-        subsets = iter(ipaddrs)
+        ipspecs = iter(ipaddrs)
     except Exception:
         pass
     else:
-        ipaddrs = IPSet()
-        for subset in subsets:
-            ipaddrs = ipaddrs | parse_ip_set(subset)
-        return ipaddrs
-    # anything else is an error
+        ipset = IPSet()
+        for ipspec in ipspecs:
+            ipset |= make_ip_set(ipspec)
+        return ipset
+    # Anything else is an error
     raise ValueError("can't convert to IPSet: %r" % (ipaddrs,))
 
 
-class IPAuthenticationPolicy(object):
-    """An IP-based authentication policy for pyramid.
+def parse_ip_set(ipaddrs):
+    """Parse a string specification into an IPSet.
 
-    This pyramid authentication policy assigns userid and/or effective
-    principals based on the originating IP address of the request.
+    This function takes a string representing a set of IP addresses, and
+    parses it into an IPSet object.  Acceptable formats for the string
+    include:
 
-    You must specify a set of IP addresses against which to match, and may
-    specify a userid and/or list of principals to apply.  For example, the
-    following would authenticate all requests from the 192.168.0.* range as
-    userid "myuser":
-
-        IPAuthenticationPolicy(["192.168.0.0/24"], "myuser")
-
-    The following would not authenticate as a particular userid, but would add
-    "local" as an effective principal for the request (along with Everyone
-    and Authenticated):
-
-        IPAuthenticationPolicy(["127.0.0.0/24"], principals=["local"])
-
-    By default this policy does not respect the X-Forwarded-For header since
-    it can be easily spoofed.  If you want to respect X-Forwarded-For then you
-    must specify a list of trusted proxies, and only forwarding declarations
-    from these proxies will be respected:
-
-        IPAuthenticationPolicy(["192.168.0.0/24"], "myuser", 
-                               proxies=["192.168.0.2"])
+        * "all":        all possible IPv4 and IPv6 addresses
+        * "local":      all local addresses of the machine
+        * "A.B.C.D"     a single IP address
+        * "A.B.C.D/N"   a network address specification
+        * "A.B.C.*"     a glob matching against all possible numbers
+        * "A.B.C.D-E"   a glob matching against a range of numbers
+        * a whitespace- or comma-separated string of the above
 
     """
+    ipset = IPSet()
+    ipaddrs = ipaddrs.lower().strip()
+    if not ipaddrs:
+        return ipset
+    for ipspec in _COMMA_OR_WHITESPACE.split(ipaddrs):
+        # The string "local" maps to all local addresses on the machine.
+        if ipspec == "local":
+            ipset.add(IPNetwork("127.0.0.0/8"))
+            for addr in get_local_ip_addresses():
+                ipset.add(addr)
+        # The string "all" maps to app IPv4 and IPv6 addresses.
+        elif ipspec == "all":
+            ipset.add(IPNetwork("0.0.0.0/0"))
+            ipset.add(IPNetwork("::"))
+        # Strings containing a "/" are assumed to be network specs
+        elif "/" in ipspec:
+            ipset.add(IPNetwork(ipspec))
+        # Strings containing a "*" or "-" are assumed to be glob patterns
+        elif "*" in ipspec or "-" in ipspec:
+            for cidr in IPGlob(ipspec).cidrs():
+                ipset.add(cidr)
+        # Anything else must be a single address
+        else:
+            ipset.add(IPAddress(ipspec))
+    return ipset
 
-    implements(IAuthenticationPolicy)
 
-    def __init__(self, ipaddrs, userid=None, principals=None, proxies=None):
-        self.ipaddrs = parse_ip_set(ipaddrs)
-        self.userid = userid
-        self.principals = principals
-        self.proxies = parse_ip_set(proxies)
-
-    def authenticated_userid(self, request):
-        return self.unauthenticated_userid(request)
-
-    def unauthenticated_userid(self, request):
-        if not check_ip_address(request, self.ipaddrs, self.proxies):
-            return None
-        return self.userid
-
-    def effective_principals(self, request):
-        principals = [Everyone]
-        if check_ip_address(request, self.ipaddrs, self.proxies):
-            if self.userid is not None:
-                principals.insert(0, self.userid)
-            principals.append(Authenticated)
-            if self.principals is not None:
-                principals.extend(self.principals)
-        return principals
-
-    def remember(self, request, principal, **kw):
-        pass
-
-    def forget(self, request):
-        pass
+def get_local_ip_addresses():
+    """Iterator yielding all local IP addresses on the machine."""
+    # XXX: how can we enumerate all interfaces on the machine?
+    # I don't really want to shell out to `ifconfig`
+    for addr in socket.gethostbyname_ex(socket.gethostname())[2]:
+        yield IPAddress(addr)
--- cornice/auth/tests.py
+++ cornice/auth/tests.py
@@ -1,17 +1,10 @@
 import unittest
 import socket
 
-from pyramid import testing
+from pyramid.testing import DummyRequest
 from pyramid.security import Everyone, Authenticated
 
-from cornice.auth.ipauth import IPAuthenticationPolicy, parse_ip_set, IPAddress
-
-# XXX: need to look up pyramid testing docs...
-def make_request(environ):
-    class request:
-        pass
-    request.environ = environ
-    return request
+from cornice.auth.ipauth import IPAuthenticationPolicy, make_ip_set, IPAddress
 
 
 class IPAuthPolicyTests(unittest.TestCase):
@@ -19,50 +12,50 @@ class IPAuthPolicyTests(unittest.TestCase):
     def test_remote_addr(self):
         policy = IPAuthenticationPolicy(["123.123.0.0/16"], "user")
         # Addresses outside the range don't authenticate
-        request = make_request({"REMOTE_ADDR": "192.168.0.1"})
+        request = DummyRequest(environ={"REMOTE_ADDR": "192.168.0.1"})
         self.assertEquals(policy.authenticated_userid(request), None)
         # Addresses inside the range do authenticate
-        request = make_request({"REMOTE_ADDR": "123.123.0.1"})
+        request = DummyRequest(environ={"REMOTE_ADDR": "123.123.0.1"})
         self.assertEquals(policy.authenticated_userid(request), "user")
-        request = make_request({"REMOTE_ADDR": "123.123.1.2"})
+        request = DummyRequest(environ={"REMOTE_ADDR": "123.123.1.2"})
         self.assertEquals(policy.authenticated_userid(request), "user")
 
     def test_noncontiguous_ranges(self):
         policy = IPAuthenticationPolicy(["123.123.0.0/16", "124.124.1.0/24"],
                                         "user")
         # Addresses outside the range don't authenticate
-        request = make_request({"REMOTE_ADDR": "192.168.0.1"})
+        request = DummyRequest(environ={"REMOTE_ADDR": "192.168.0.1"})
         self.assertEquals(policy.authenticated_userid(request), None)
-        request = make_request({"REMOTE_ADDR": "124.124.0.1"})
+        request = DummyRequest(environ={"REMOTE_ADDR": "124.124.0.1"})
         self.assertEquals(policy.authenticated_userid(request), None)
         # Addresses inside the range do authenticate
-        request = make_request({"REMOTE_ADDR": "123.123.0.1"})
+        request = DummyRequest(environ={"REMOTE_ADDR": "123.123.0.1"})
         self.assertEquals(policy.authenticated_userid(request), "user")
-        request = make_request({"REMOTE_ADDR": "124.124.1.2"})
+        request = DummyRequest(environ={"REMOTE_ADDR": "124.124.1.2"})
         self.assertEquals(policy.authenticated_userid(request), "user")
 
     def test_x_forwarded_for(self):
         policy = IPAuthenticationPolicy(["123.123.0.0/16"], "user",
                               proxies=["124.124.0.0/24"])
         # Requests without X-Forwarded-For work as normal
-        request = make_request({"REMOTE_ADDR": "192.168.0.1"})
+        request = DummyRequest(environ={"REMOTE_ADDR": "192.168.0.1"})
         self.assertEquals(policy.authenticated_userid(request), None)
-        request = make_request({"REMOTE_ADDR": "123.123.0.1"})
+        request = DummyRequest(environ={"REMOTE_ADDR": "123.123.0.1"})
         self.assertEquals(policy.authenticated_userid(request), "user")
-        # Requests with untrusted X-Forwarded-For don't authenticate 
-        request = make_request({"REMOTE_ADDR": "192.168.0.1",
+        # Requests with untrusted X-Forwarded-For don't authenticate
+        request = DummyRequest(environ={"REMOTE_ADDR": "192.168.0.1",
                                "HTTP_X_FORWARDED_FOR": "123.123.0.1"})
         self.assertEquals(policy.authenticated_userid(request), None)
-        # Requests from single trusted proxy do authenticate 
-        request = make_request({"REMOTE_ADDR": "124.124.0.1",
+        # Requests from single trusted proxy do authenticate
+        request = DummyRequest(environ={"REMOTE_ADDR": "124.124.0.1",
                                "HTTP_X_FORWARDED_FOR": "123.123.0.1"})
         self.assertEquals(policy.authenticated_userid(request), "user")
-        # Requests from chain of trusted proxies do authenticate 
-        request = make_request({"REMOTE_ADDR": "124.124.0.2",
+        # Requests from chain of trusted proxies do authenticate
+        request = DummyRequest(environ={"REMOTE_ADDR": "124.124.0.2",
                           "HTTP_X_FORWARDED_FOR": "123.123.0.1, 124.124.0.1"})
         self.assertEquals(policy.authenticated_userid(request), "user")
         # Requests with untrusted proxy in chain don't authenticate
-        request = make_request({"REMOTE_ADDR": "124.124.0.1",
+        request = DummyRequest(environ={"REMOTE_ADDR": "124.124.0.1",
                           "HTTP_X_FORWARDED_FOR": "123.123.0.1, 192.168.0.1"})
         self.assertEquals(policy.authenticated_userid(request), None)
 
@@ -70,17 +63,16 @@ class IPAuthPolicyTests(unittest.TestCase):
         policy = IPAuthenticationPolicy(["123.123.0.0/16"],
                                         principals=["test"])
         # Addresses outside the range don't get metadata set
-        request = make_request({"REMOTE_ADDR": "192.168.0.1"})
+        request = DummyRequest(environ={"REMOTE_ADDR": "192.168.0.1"})
         self.assertEquals(policy.effective_principals(request), [Everyone])
-                          
         # Addresses inside the range do get metadata set
-        request = make_request({"REMOTE_ADDR": "123.123.0.1"})
+        request = DummyRequest(environ={"REMOTE_ADDR": "123.123.0.1"})
         self.assertEquals(policy.effective_principals(request),
                           [Everyone, Authenticated, "test"])
 
-    def test_parse_ip_set(self):
+    def test_make_ip_set(self):
         def is_in(ipaddr, ipset):
-            ipset = parse_ip_set(ipset)
+            ipset = make_ip_set(ipset)
             return IPAddress(ipaddr) in ipset
         #  Test individual IPs
         self.assertTrue(is_in("127.0.0.1", "127.0.0.1"))
@@ -90,15 +82,14 @@ class IPAuthPolicyTests(unittest.TestCase):
         self.assertTrue(is_in("127.0.1.2", "127.0.*.*"))
         self.assertTrue(is_in("127.0.0.1", "127.0.0.*"))
         self.assertFalse(is_in("127.0.1.2", "127.0.0.*"))
+        self.assertTrue(is_in("127.0.0.1", "127.0.0.1-5"))
+        self.assertTrue(is_in("127.0.0.5", "127.0.0.1-5"))
+        self.assertFalse(is_in("127.0.0.6", "127.0.0.1-5"))
         #  Test networks
         self.assertTrue(is_in("127.0.0.1", "127.0.0.0/8"))
         self.assertTrue(is_in("127.0.0.1", "127.0.0.0/16"))
         self.assertTrue(is_in("127.0.0.1", "127.0.0.0/24"))
         self.assertFalse(is_in("127.0.1.2", "127.0.0.0/24"))
-        #  Test ranges
-        self.assertTrue(is_in("127.0.0.1", "127.0.0.1-127.0.0.5"))
-        self.assertTrue(is_in("127.0.0.5", "127.0.0.1-127.0.0.5"))
-        self.assertFalse(is_in("127.0.0.6", "127.0.0.1-127.0.0.5"))
         #  Test literal None
         self.assertFalse(is_in("127.0.0.1", None))
         #  Test special strings
@@ -118,9 +109,8 @@ class IPAuthPolicyTests(unittest.TestCase):
         self.assertFalse(is_in("127.0.0.2", ips))
         self.assertTrue(is_in("127.0.1.2", ips))
         #  Test with a string-list of stuff
-        ips = "127.0.0.1, 127.0.1.*"
+        ips = "123.123.0.0/16 local"
         self.assertTrue(is_in("127.0.0.1", ips))
         self.assertTrue(is_in("127.0.1.1", ips))
-        self.assertFalse(is_in("127.0.0.2", ips))
-        self.assertTrue(is_in("127.0.1.2", ips))
-
+        self.assertTrue(is_in("123.123.1.1", ips))
+        self.assertFalse(is_in("124.0.0.1", ips))
