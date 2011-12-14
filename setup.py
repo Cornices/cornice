4b1662d7fb4fb1ec63391d5c0a235470782db136
Alexis Metaireau <alexis@notmyidea.org>
2011-12-14 15:02:41 +0100

    rename README.txt to README.rst

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-with open(os.path.join(here, 'README.txt')) as f:
+with open(os.path.join(here, 'README.rst')) as f:
     README = f.read()
 
 with open(os.path.join(here, 'CHANGES.txt')) as f:
