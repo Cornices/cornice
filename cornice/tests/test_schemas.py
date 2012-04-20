cae1f8b787f4cc461b9658e888009d51becd8bee
Alexis Metaireau <alexis@notmyidea.org>
2012-04-20 12:22:54 +0200

    use a specific schema for the tests of cornice.schema

--- cornice/tests/test_schemas.py
+++ cornice/tests/test_schemas.py
@@ -1,14 +1,30 @@
 from unittest import TestCase
 from cornice.schemas import CorniceSchema
-from cornice.tests.validationapp import FooBarSchema
 
+try:
+    from colander import (
+        MappingSchema,
+        SchemaNode,
+        String,
+    )
+    COLANDER = True
+except ImportError:
+    COLANDER = False
 
-class TestSchemas(TestCase):
+if COLANDER:
 
-    def test_colander_integration(self):
-        schema = CorniceSchema.from_colander(FooBarSchema)
-        body_fields = schema.get_attributes(location="body")
-        qs_fields = schema.get_attributes(location="querystring")
+    class TestingSchema(MappingSchema):
+        foo = SchemaNode(String(), type='str')
+        bar = SchemaNode(String(), type='str', location="body")
+        baz = SchemaNode(String(), type='str', location="querystring")
 
-        self.assertEquals(len(body_fields), 4)
-        self.assertEquals(len(qs_fields), 1)
+    class TestSchemas(TestCase):
+
+        def test_colander_integration(self):
+            # not specifying body should act the same way as specifying it
+            schema = CorniceSchema.from_colander(TestingSchema)
+            body_fields = schema.get_attributes(location="body")
+            qs_fields = schema.get_attributes(location="querystring")
+
+            self.assertEquals(len(body_fields), 2)
+            self.assertEquals(len(qs_fields), 1)
