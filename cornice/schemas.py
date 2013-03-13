6311276d8d437e0b8e8ef60cb55fd60ca8c22739
Luper Rouch <luper.rouch@gmail.com>
2013-03-13 23:39:10 +0100

    supporting imperative colander schemas

--- cornice/schemas.py
+++ cornice/schemas.py
@@ -11,10 +11,14 @@ class CorniceSchema(object):
         self._c_schema = _colander_schema
 
     def bind_attributes(self, request=None):
-        if request:
-            self._attributes = self._c_schema().bind(request=request).children
+        if callable(self._c_schema):
+            schema = self._c_schema()
         else:
-            self._attributes = self._c_schema().children
+            schema = self._c_schema
+        if request:
+            self._attributes = schema.bind(request=request).children
+        else:
+            self._attributes = schema.children
 
     def get_attributes(self, location=("body", "headers", "querystring"),
                        required=(True, False),
