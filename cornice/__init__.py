bd1f72dadd3c95163b61be98f475e048c8857b08
Alexis Métaireau <alexis@notmyidea.org>
2013-11-12 13:09:33 +0100

    Bump version number

--- cornice/__init__.py
+++ cornice/__init__.py
@@ -18,7 +18,7 @@ from pyramid.httpexceptions import HTTPNotFound, HTTPForbidden
 from pyramid.security import NO_PERMISSION_REQUIRED
 
 logger = logging.getLogger('cornice')
-__version__ = 0.16.1
+__version__ = 0.17
 
 
 def add_renderer_globals(event):
--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ package_data = {
         "cornice/+package+/*.*"]}
 
 setup(name='cornice',
-      version='0.16.1',
+      version='0.17',
       description='Define Web Services in Pyramid.',
       long_description=README + '\n\n' + CHANGES,
       license='MPLv2.0',
