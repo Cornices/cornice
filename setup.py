80e92bb6107bcac98ca271b9ad4fb64aa4aec3a2
Alexis Métaireau <alexis@notmyidea.org>
2014-08-28 12:45:28 +0200

    Back to dev: 0.18

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ package_data = {
         "cornice/+package+/*.*"]}
 
 setup(name='cornice',
-      version='0.17',
+      version='0.18',
       description='Define Web Services in Pyramid.',
       long_description=README + '\n\n' + CHANGES,
       license='MPLv2.0',
