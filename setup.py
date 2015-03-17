581eea698b5d672b7b76030c7ea6e9602338c93c
Alexis Métaireau <alexis@notmyidea.org>
2015-03-17 18:20:37 +0100

    Preparing release 0.20.0

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ package_data = {
         "cornice/+package+/*.*"]}
 
 setup(name='cornice',
-      version='0.20.dev0',
+      version='0.20.0',
       description='Define Web Services in Pyramid.',
       long_description=README + '\n\n' + CHANGES,
       license='MPLv2.0',
