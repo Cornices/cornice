6e48b700630a76cdfd37b19d58a8c93cf758ef06
Karol Tomala <ktomala@tropicoders.pl>
2015-04-28 17:08:35 +0200

    Fix flake8 warnings.

--- cornice/resource.py
+++ cornice/resource.py
@@ -18,41 +18,43 @@ def resource(depth=1, **kw):
     will be used as such. You can also prefix them by "collection_" and they
     will be treated as HTTP methods for the given collection path
     (collection_path), if any.
-    
+
     Here is an example::
 
         @resource(collection_path='/users', path='/users/{id}')
-        
+
     You can also add any other prefixed path like e.g. "activate_path".
     This may be useful when defining sub-resources.
 
     Here is an example::
 
-        @resource(collection_path='/users', path='/users/{id}', 
+        @resource(collection_path='/users', path='/users/{id}',
                   activate_path='/users/{id}/activate')
     """
     def wrapper(klass):
         services = {}
 
-        path_keys = [x[:len(x)-len('path')] for x in list(kw) if x.endswith('_path')]
+        path_keys = [x[:len(x)-len('path')] for x in list(kw)
+                     if x.endswith('_path')]
         if len(path_keys) > 0:
             prefixes = tuple(path_keys + [''])
         else:
             prefixes = ('',)
-        
+
         def filter_prefixes(prefix, x):
             """Filters out keys for prefixes other than specified prefix.
             """
             other = set(prefixes) - set(['', prefix])
             return len([y for y in other if x.startswith(y)]) == 0
-        
+
         for prefix in prefixes:
 
             # get clean view arguments
             service_args = {}
-            
-            filter_other = filter(functools.partial(filter_prefixes, prefix), list(kw))
-            
+
+            filter_other = filter(functools.partial(filter_prefixes, prefix),
+                                  list(kw))
+
             for k in filter_other:
                 if k.startswith(prefix):
                     service_args[k[len(prefix):]] = kw[k]
