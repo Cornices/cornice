ef4fd62dd8159d2b65c0122e0fa2f016ef11bb34
Rickert Mulder <circlingthesun@gmail.com>
2015-04-30 13:06:29 +0200

    Add test for warning

--- cornice/tests/test_resource.py
+++ cornice/tests/test_resource.py
@@ -71,10 +71,24 @@ class User(object):
         return dict(type=repr(self.context))
 
 
+class TestResourceWarning(TestCase):
+    @mock.patch('warnings.warn')
+    def test_path_clash(self, mocked_warn):
+        @resource(collection_path='/badthing/{id}', path='/badthing/{id}',
+                  name='bad_thing_service')
+        class BadThing(object):
+            def __init__(self, request, context=None):
+                pass
+
+        msg = "Warning: collection_path and path are not distinct."
+        mocked_warn.assert_called_with(msg)
+
+
 class TestResource(TestCase):
 
     def setUp(self):
         from pyramid.renderers import JSONP
+
         self.config = testing.setUp()
         self.config.add_renderer('jsonp', JSONP(param_name='callback'))
         self.config.include("cornice")
