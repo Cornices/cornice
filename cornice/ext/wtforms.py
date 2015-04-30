25a3eaf9ce6d6f57fb7bed3cd8b6d43835abd267
Karol Tomala <karol.tomala@daftcode.pl>
2015-04-30 15:06:31 +0200

    Fix flake8 warning.

--- cornice/ext/wtforms.py
+++ cornice/ext/wtforms.py
@@ -2,6 +2,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
+
 def validate_wtforms_schema(form_schema, with_matchdict=False):
     """Validates JSON data through WTForms schema.
 
