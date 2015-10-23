c738444a59430e8ca1b54fb8ef020ec47753bd6e
Kit Randel <kit@nocturne.net.nz>
2015-10-23 16:29:04 +1300

    Refactor directive to setUp.

--- cornice/tests/ext/test_sphinxext.py
+++ cornice/tests/ext/test_sphinxext.py
@@ -18,6 +18,16 @@ class TestUtil(TestCase):
 
 class TestServiceDirective(TestCase):
 
+    def setUp(self):
+        super(TestServiceDirective, self).setUp()
+        param = mock.Mock()
+        param.document.settings.env.new_serialno.return_value = 1
+
+        self.directive = ServiceDirective(
+            'test', [], {}, [], 1, 1, 'test', param, 1)
+        self.directive.options['app'] = 'cornice.tests.ext.dummy'
+        self.directive.options['services'] = ['users', "thing_service"]
+
     def test_module_reload(self):
         directive = ServiceDirective(
             'test', [], {}, [], 1, 1, 'test', mock.Mock(), 1)
@@ -26,24 +36,12 @@ class TestServiceDirective(TestCase):
         self.assertEqual(ret, [])
 
     def test_dummy(self):
-        param = mock.Mock()
-        param.document.settings.env.new_serialno.return_value = 1
-        directive = ServiceDirective(
-            'test', [], {}, [], 1, 1, 'test', param, 1)
-        directive.options['app'] = 'cornice.tests.ext.dummy'
-        directive.options['services'] = ['users', "thing_service"]
-        ret = directive.run()
+        ret = self.directive.run()
         self.assertEqual(len(ret), 2)
         self.assertIn('Users service at', str(ret[0]))
         self.assertIn('Thing_Service service at ', str(ret[1]))
 
     def test_string_validator_resolved(self):
         # A validator defined as a string should be parsed as an obj.
-        param = mock.Mock()
-        param.document.settings.env.new_serialno.return_value = 1
-        directive = ServiceDirective(
-            'test', [], {}, [], 1, 1, 'test', param, 1)
-        directive.options['app'] = 'cornice.tests.ext.dummy'
-        directive.options['services'] = ['users', "thing_service"]
-        ret = directive.run()
+        ret = self.directive.run()
         self.assertNotIn("str(object='') -> string", str(ret[0]))
