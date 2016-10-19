0988da81d9c9b853d82c39eceba0fb3a3617d8ef
Mathieu Leplatre <mathieu@mozilla.com>
2016-10-19 12:08:13 +0200

    Remove useless import

--- cornice/service.py
+++ cornice/service.py
@@ -2,7 +2,6 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 import functools
-import warnings
 from pyramid.response import Response
 from cornice.validators import (
     DEFAULT_VALIDATORS,
