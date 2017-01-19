8ff4a2e29a2b54e798dc1eae81158875a15c13b3
Mathieu Leplatre <mathieu@mozilla.com>
2017-01-19 16:32:30 +0100

    Back to development: 2.5.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ entry_points = ""
 package_data = {}
 
 setup(name='cornice',
-      version='2.4.0',
+      version='2.5.0.dev0',
       description='Define Web Services in Pyramid.',
       long_description=README + '\n\n' + CHANGES,
       license='MPLv2.0',
