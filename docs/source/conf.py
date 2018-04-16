cfb4a57f694c620557a8d4d924375f50116a9fe7
Gasper Vozel <gasper.vozel@niteoweb.com>
2018-04-16 16:18:07 +0200

    Fix code block in tutorial.rst
    Also, few minor modifications in `docs/source/conf.py`.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,28 +1,32 @@
 # flake8: noqa
 # -*- coding: utf-8 -*-
-import sys, os
+import datetime
+import os
+import sys
+import pkg_resources
 try:
     import mozilla_sphinx_theme
 except ImportError:
-    print("please install the 'mozilla-sphinx-theme' distribution")
+    print('please install the \'mozilla-sphinx-theme\' distribution')
 
-sys.path.insert(0, os.path.abspath("../.."))  # include cornice from the source
+sys.path.insert(0, os.path.abspath('../..'))  # include cornice from the source
 extensions = ['sphinx.ext.autodoc']
 
 templates_path = ['_templates']
 source_suffix = '.rst'
 master_doc = 'index'
 project = u'Cornice'
-copyright = u'2011-2016, Mozilla Services'
+this_year = datetime.datetime.now().year
+copyright = u'2011-{}, Mozilla Services'.format(this_year)
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
 # built documents.
 #
 # The short X.Y version.
-version = '2.3'
+version =  pkg_resources.get_distribution('cornice').version
 # The full version, including alpha/beta/rc tags.
-release = '2.3.0'
+release = version
 
 exclude_patterns = []
 
