111d6939e97a4307f9166594fd5f794b43b413b4
Rémy HUBSCHER <remy@chefclub.tv>
2019-01-23 16:56:26 +0100

    Do not trigger a request.localizer when not in use. Fixes #505

--- cornice/pyramidhook.py
+++ cornice/pyramidhook.py
@@ -149,7 +149,10 @@ def wrap_request(event):
         setattr(request, 'validated', {})
 
     if not hasattr(request, 'errors'):
-        setattr(request, 'errors', Errors(localizer=request.localizer))
+        if "available_languages" in request.registry.settings:
+            setattr(request, 'errors', Errors(localizer=request.localizer))
+        else:
+            setattr(request, 'errors', Errors())
 
     if not hasattr(request, 'info'):
         setattr(request, 'info', {})
