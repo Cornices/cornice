74999ee8f6950371d8bf182b1b2ce968c65553b4
Mathieu Leplatre <mathieu@mozilla.com>
2019-01-24 10:36:30 +0100

    Preparing release 3.5.1

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ entry_points = ""
 package_data = {}
 
 setup(name='cornice',
-      version='3.6.0.dev0',
+      version='3.5.1',
       description='Define Web Services in Pyramid.',
       long_description=README + '\n\n' + CHANGES,
       license='MPLv2.0',
