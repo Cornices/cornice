bafcfe45133d71ddc80f3bc99026e9353b2a5fcf
Andrew Birnberg <birnbera@users.noreply.github.com>
2019-06-27 14:20:26 -0700

    fix line length for flake

--- cornice/validators/__init__.py
+++ cornice/validators/__init__.py
@@ -51,7 +51,10 @@ def extract_cstruct(request):
     """
     is_json = re.match('^application/(.*?)json$', str(request.content_type))
 
-    if request.content_type in ('application/x-www-form-urlencoded', 'multipart/form-data'):
+    if request.content_type in (
+        'application/x-www-form-urlencoded', 
+        'multipart/form-data'
+    ):
         body = request.POST.mixed()
     elif request.content_type and not is_json:
         body = request.body
