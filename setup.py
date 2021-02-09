01054b01746ae08cb4bb37cda8feff62d057ed7a
Mathieu Leplatre <mathieu@mozilla.com>
2021-02-09 11:16:21 +0100

    Force Pyramid < 2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(os.path.join(here, 'README.rst')) as f:
 with open(os.path.join(here, 'CHANGES.txt')) as f:
     CHANGES = f.read()
 
-requires = ['pyramid>=1.7', 'venusian']
+requires = ['pyramid>=1.7,<2', 'venusian']
 
 entry_points = ""
 package_data = {}
