f0bdb03b733c81a8ac325ac1146bf662b7105367
Mathieu Leplatre <mathieu@leplat.re>
2021-04-06 11:36:11 +0200

    Preparing release 5.2.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ entry_points = ""
 package_data = {}
 
 setup(name='cornice',
-      version='5.2.0.dev0',
+      version='5.2.0',
       description='Define Web Services in Pyramid.',
       long_description=README + '\n\n' + CHANGES,
       license='MPLv2.0',
